19198f481b1473bc9f33cd936e98fff35df810ff
MoisesMJr <moisesmanonjr@hotmail.com>
2022-05-29 21:42:05 -0400

    fixed bug that stopped reset from functioning whenever start button pressed multiple times

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-var seconds = 00;
-var tens = 00;
-var appendTens = document.getElementById("tens");
-var appendSeconds = document.getElementById("seconds");
-var buttonStart = document.getElementById("button-start");
-var buttonStop = document.getElementById("button-stop");
-var buttonReset = document.getElementById("button-reset");
-var interval; //to store timer
+let seconds = 00;
+let tens = 00;
+let appendTens = document.getElementById("tens");
+let appendSeconds = document.getElementById("seconds");
+let buttonStart = document.getElementById("button-start");
+let buttonStop = document.getElementById("button-stop");
+let buttonReset = document.getElementById("button-reset");
+let interval; //to store timer
+let timerRunning = false;
 
 // this function will run when click on start
 
 function startTimer(){
-    tens++
+    if(timerRunning === false){
+        timerRunning = true; 
+    }
+}
 
-    if(tens<9){
-        appendTens.innerHTML = "0" + tens;
-    }
-    if(tens>9){
-        appendTens.innerHTML = tens;
-    }
-    if(tens>99){
-        seconds++
-        appendSeconds.innerHTML = "0" + seconds;
-        tens = 0;
-        appendTens.innerHTML = "0" + 0;
-    }
-    if(seconds>9){
-        appendSeconds.innerHTML = seconds;
+function startTime(){
+    if (timerRunning === true) {
+        tens++
+        if(tens<9){
+            appendTens.innerHTML = "0" + tens;
+        }
+        if(tens>9){
+            appendTens.innerHTML = tens;
+        }
+        if(tens>99){
+            seconds++
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+        }
+        if(seconds>9){
+            appendSeconds.innerHTML = seconds;
+        }
     }
 }
 
 buttonStart.onclick = function(){
-    interval = setInterval(startTimer);
+    interval = setInterval(startTime);
+    startTimer();
 }
 buttonStop.onclick = function(){
     clearInterval(interval);
+    timerRunning = false;
 }
 buttonReset.onclick = function(){
-    clearInterval(interval);
     tens = "00";
     seconds = "00";
     appendSeconds.innerHTML = seconds;
